80eab4cf85d1cee1235e1aea0306124bdabf69f3
FumaniGolele <105161844+FumaniGolele@users.noreply.github.com>
2022-06-02 13:24:53 +0200

    Add files via upload

--- codetwo.js
+++ codetwo.js
@@ -1,32 +1,40 @@
 
 localStorage.setItem("ExecuteCheck","false");
 
-
 $(document).ready(function() {
-
-    // getUpdates();
-         GetDataSensor4();
-         GetDataSensor2();
-         GetDataSensor3();
-         GetData();
-
-     // check for new updates
-     setInterval('GetData()',30000);
-     setInterval('GetDataSensor2()',30000);
-     setInterval('GetDataSensor3()',30000);
-     setInterval('GetDataSensor4()',30000);
+        
+     setInterval('GetData()',35000);
+     setInterval('GetSensor2()',35000);
+     setInterval('GetSensor3()',35000);
+     setInterval('GetSensor4()',35000);
 
  });
 
- /*function getUpdates() {
+ function execute(message){
+newmessage = message.replaceAll(' ', '+');
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api.telegram.org/bot5405026731:AAFmx1cBTCy-GEdfb4ICjGOIFj5PYnnWJ-o/sendMessage?chat_id=-1001726240418&text='+newmessage, true)
+request.send();
+alert(message);
+}
 
-     // get the data with a webservice call
-      
-     $.getJSON('http://api.thingspeak.com/channels/1708546/feed/last.json?callback=?',function(data) {
-         alert(data.field1);
-     });
+function enable()
+{
+   localStorage.setItem("ExecuteCheck","true");
+   execute(
+       "Mine Notifications Enabled"
+   );
 
- }*/
+}
+
+function disable()
+{
+   localStorage.setItem("ExecuteCheck", "false");
+   execute(
+       "Mine Notifications Disabled"
+   );
+}
+
  function GetData()
  {
      var url = 'https://api.thingspeak.com/channels/1708546/feed/last.json?callback=?';
@@ -42,21 +50,19 @@ $(document).ready(function() {
          var inspectSecond = parseFloat(data.field2);
          if (localStorage.getItem("ExecuteCheck") == "true")
          {
-
-         
          if(inspect >= 40){
-             execute("Sensor 1 temperature is greater than 40");
+             execute("Sensor 1 is above maximum threshold");
          }
          else if (inspect < 0)
          {
-             execute("Sensor 1 temperature is less than 0");
+             execute("Sensor 1 is below minimum threshold");
          }
          if(inspectSecond >= 40){
-             execute("Sensor 1 humidity is greater than 40");
+             execute("Sensor 1 humidity is above threshold");
          }
          else if (inspectSecond < 0)
          {
-             execute("Sensor 1 humidity is less than 0");
+             execute("Sensor 1 humidity is less than threshold");
          }
          
          
@@ -65,7 +71,9 @@ $(document).ready(function() {
         }
      });
  }
- function GetDataSensor2()
+
+
+ function GetSensor2()
  {
      var url = 'https://api.thingspeak.com/channels/1737929/feed/last.json?callback=?';
      $.ajax
@@ -79,20 +87,21 @@ $(document).ready(function() {
              var inspect = parseFloat(data.field1);
          var inspectSecond = parseFloat(data.field2);
          if (localStorage.getItem("ExecuteCheck") == "true")
+         if (localStorage.getItem("ExecuteCheck") == "true")
          {
          if(inspect >= 40){
-             execute("Sensor 2 temperature is greater than 40");
+             execute("Sensor 2 is above maximum threshold");
          }
          else if (inspect < 0)
          {
-             execute("Sensor 2 temperature is less than 0");
+             execute("Sensor 2 is below minimum threshold");
          }
          if(inspectSecond >= 40){
-             execute("Sensor 2 humidity is greater than 40");
+             execute("Sensor 2 humidity is above threshold");
          }
          else if (inspectSecond < 0)
          {
-             execute("Sensor 2 humidity is less than 0");
+             execute("Sensor 2 humidity is less than threshold");
          }
          
          
@@ -100,7 +109,7 @@ $(document).ready(function() {
         }
      });
  }
- function GetDataSensor3()
+ function GetSensor3()
  {
      var url = 'https://api.thingspeak.com/channels/1738066/feed/last.json?callback=?';
      $.ajax
@@ -115,28 +124,31 @@ $(document).ready(function() {
          var inspectSecond = parseFloat(data.field2);
          if (localStorage.getItem("ExecuteCheck") == "true")
          {
-         if(inspect >= 40){
-             execute("Sensor 3 temperature is greater than 40");
-         }
-         else if (inspect < 0)
-         {
-             execute("Sensor 3 temperature is less than 0");
-         }
-         if(inspectSecond >= 40){
-             execute("Sensor 3 humidity is greater than 40");
-         }
-         else if (inspectSecond < 0)
-         {
-             execute("Sensor 3 humidity is less than 0");
-         }
-         
-         
-         }
+            if (localStorage.getItem("ExecuteCheck") == "true")
+            {
+            if(inspect >= 40){
+                execute("Sensor 3 is above maximum threshold");
+            }
+            else if (inspect < 0)
+            {
+                execute("Sensor 3 is below minimum threshold");
+            }
+            if(inspectSecond >= 40){
+                execute("Sensor 3 humidity is above threshold");
+            }
+            else if (inspectSecond < 0)
+            {
+                execute("Sensor 3 humidity is less than threshold");
+            }
+            
+            
+            }
         }
          
-     });
+     }
+    });
  }
- function GetDataSensor4()
+ function GetSensor4()
  {
      var url = 'https://api.thingspeak.com/channels/1738082/feed/last.json?callback=?';
      $.ajax
@@ -146,54 +158,29 @@ $(document).ready(function() {
          contentType: "application/json",
          //dataType: "json",
          //crossDomain: true,
-         success: function (data, textStatus, xhr) {
-             
+         success: function (data, textStatus, xhr) { 
          var inspect = parseFloat(data.field1);
          var inspectSecond = parseFloat(data.field2);
          if (localStorage.getItem("ExecuteCheck") == "true")
          {
-         if(inspect >= 40){
-             execute("Sensor 4 temperature is greater than 40");
-         }
-         else if (inspect < 0)
-         {
-             execute("Sensor 4 temperature is less than 0");
-         }
-         if(inspectSecond >= 40){
-             execute("Sensor 4 humidity is greater than 40");
-         }
-         else if (inspectSecond < 0)
-         {
-             execute("Sensor 4 humidity is less than 0");
-         }
-         
-         
+            if (localStorage.getItem("ExecuteCheck") == "true")
+            {
+            if(inspect >= 40){
+                execute("Sensor 4 is above maximum threshold");
+            }
+            else if (inspect < 0)
+            {
+                execute("Sensor 4 is below minimum threshold");
+            }
+            if(inspectSecond >= 40){
+                execute("Sensor 4 humidity is above threshold");
+            }
+            else if (inspectSecond < 0)
+            {
+                execute("Sensor 4 humidity is less than threshold");
+            }
+            }
          }
         }
      });
  }
-
- function execute(message){
-     newmessage = message.replaceAll(' ', '+');
- var request = new XMLHttpRequest();
- request.open('GET', 'https://api.telegram.org/bot5405026731:AAFmx1cBTCy-GEdfb4ICjGOIFj5PYnnWJ-o/sendMessage?chat_id=-1001726240418&text='+newmessage, true)
- request.send();
- alert(message);
-}
-
-function enable()
-{
-    localStorage.setItem("ExecuteCheck","true");
-    execute(
-        "Notifications Enabled"
-    );
-
-}
-
-function disable()
-{
-    localStorage.setItem("ExecuteCheck", "false");
-    execute(
-        "notifications Disabled"
-    );
-}
